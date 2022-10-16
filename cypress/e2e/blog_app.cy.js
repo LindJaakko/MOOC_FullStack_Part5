08eb440a6e7ed34e2dc6e4c30cd829ae215ea7de
LindJaakko <lind.jaakko@gmail.com>
2022-10-16 22:30:57 +0300

    Finalized exercise 5.17

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function () {
+    cy.contains('log in to application')
+    cy.contains('username')
+    cy.contains('password')
   })
 })
